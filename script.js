85d759c779f584fb54a1fea4b2fdffa7febf08c7
Christina <christinalmoss@gmail.com>
2021-01-04 19:59:48 -0600

    Add initial elements to page, made functional start button to disappear upon click

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 var qIndex = 0;
 var currentqIndex = 0;
-var startBtn = document.querySelector("#startButton")
-var timer = document.querySelector("#timer")
+var startBtn = document.querySelector("#startButton");
+var timer = document.querySelector("#timer");
+var question = document.querySelector("#question");
+var quizWindow = document.querySelector("#quizWindow");
+// var hideStart = document.querySelector("#startOnly");
+
+function startQuiz() {
+
+    // hides start button and warning text when button is clicked
+
+    document.querySelector("#startOnly").style.visibility = "hidden";
+
+    // show quizWindow - want it to be hidden by default
+    
+    // add content to quiz window
+};
+
+startBtn.addEventListener("click", startQuiz)
